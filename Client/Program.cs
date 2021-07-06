69b8387d0728babbaf2984d696305f46d8145ff6
Michael Lambert <milambert@hotmail.com>
2021-07-06 09:24:30 -0700

    Start of Auth

--- Client/Program.cs
+++ Client/Program.cs
@@ -3,6 +3,7 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Azure.Functions.Authentication.WebAssembly;
 
 namespace Client
 {
@@ -14,6 +15,7 @@ namespace Client
             builder.RootComponents.Add<App>("app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["API_Prefix"] ?? builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddStaticWebAppsAuthentication();
 
             await builder.Build().RunAsync();
         }
